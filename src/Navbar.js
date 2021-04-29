35790299b29629f7bcfdcec30d201dd185a9c567
vishwa-akshat <akvs0707@gmail.com>
2021-04-29 21:57:35 +0530

    add navbar and font

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import 'rc-slider/assets/index.css';
+ 
+import React, { Component } from 'react'
+import Slider, { Range } from 'rc-slider';
+
+import './Navbar.css';
+
+export default class Navbar extends Component {
+    render() {
+        const {level, changeLevel} = this.props;
+        return (
+            <header className="Navbar"> 
+                <div className="logo">
+                    <a href="#">reactcolorpicker</a>
+                </div>
+                <div className="slider-container">
+                    <span>Level: {level}</span>
+                    <div className="slider">
+                        <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel}/>
+                    </div>      
+                </div> 
+            </header>
+        )
+    }
+}
