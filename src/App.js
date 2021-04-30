57dca2eea211e0877633d2fadc445f44ebd281bc
vishwa-akshat <akvs0707@gmail.com>
2021-04-30 18:46:42 +0530

    add palette list and links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+
 import Palette from './Palette';
 import { generatePalette } from './colorHelpers';
 import seedColors from './seedColors';
-import {Route, Switch} from 'react-router-dom';
-import { Component } from 'react';
+import PaletteList from './PaletteList';
+
 
 class App extends Component {
   findPalette(id){
@@ -13,7 +16,7 @@ class App extends Component {
   render(){
     return (
       <Switch>
-        <Route exact path="/" render={()=><h1>PALETTE LIST GOES HERE</h1>}/>
+        <Route exact path="/" render={()=><PaletteList palettes={seedColors} />}/>
         <Route exact path="/palette/:id" render={(routeProps)=><Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))}/>}/>
       </Switch>
       // <div >
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
+import {Link} from 'react-router-dom';
 
 import './Navbar.css';
 
@@ -29,7 +30,7 @@ export default class Navbar extends Component {
         return (
             <header className="Navbar">
                 <div className="logo">
-                    <a href="#">reactcolorpicker</a>
+                    <Link to="/">reactcolorpicker</Link>
                 </div>
                 <div className="slider-container">
                     <span>Level: {level}</span>
