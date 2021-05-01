50a27fb3fe4a2ee904c5d26a44463529e3085613
vishwa-akshat <akvs0707@gmail.com>
2021-05-01 12:29:13 +0530

    Add link palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {withStyles} from '@material-ui/styles';
 
 import MiniPalette from './MiniPalette';
@@ -35,6 +35,9 @@ const styles ={
     }
 }
 class PaletteList extends Component {
+    goToPalette(id){
+        this.props.history.push(`/palette/${id}`)
+    }
     render() {
         const {palettes, classes} = this.props;
         return (
@@ -45,7 +48,7 @@ class PaletteList extends Component {
                     </nav>
                     <div className={classes.palettes}>
                         {palettes.map(palette =>(
-                            <MiniPalette {...palette}/>
+                            <MiniPalette {...palette} handleClick={()=>this.goToPalette(palette.id)}/>
                         ) )}
                     </div>
                 </div>
